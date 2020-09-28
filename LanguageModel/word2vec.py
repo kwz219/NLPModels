c562241b7ee6c0ce6621d29486dfac6c384702b1
ikang <1007888882@qq.com>
2020-09-28 15:56:30 +0800

    word2vec and word2vec trainer(简单版实现)

--- LanguageModel/word2vec.py
+++ LanguageModel/word2vec.py
@@ -6,8 +6,9 @@ class Word2vec(nn.Module):
         super(Word2vec, self).__init__()
         self.vocab_size=vocab_size
         self.emb_size=emb_size
-        self.inemb=nn.Linear(vocab_size,emb_size)
-        self.outemb=nn.Linear(emb_size,vocab_size)
+        "注意,若是采用Linear层代替nn.Embedding，那么不应该使用bias,且输入需转换为one-hot 格式"
+        self.inemb=nn.Linear(vocab_size,emb_size,bias=False)
+        self.outemb=nn.Linear(emb_size,vocab_size,bias=False)
     def forward(self,x):
         #x:[batch_size,vocab_size]
         X=self.inemb(x)
--- Trainer/Word2vecTrainer.py
+++ Trainer/Word2vecTrainer.py
@@ -4,20 +4,24 @@ import torch.nn as nn
 import matplotlib.pyplot as plt
 import torch
 import torch.optim as optim
+
 def random_batch(skip_grams):
     random_inputs = []
     random_labels = []
     random_index = np.random.choice(range(len(skip_grams)), batch_size, replace=False)
     for i in random_index:
+        #用np.eye函数将输入转换为one-hot编码
         random_inputs.append(np.eye(voc_size)[skip_grams[i][0]])  # target
         random_labels.append(skip_grams[i][1])  # context word
     return random_inputs, random_labels
 
 if __name__ =="__main__":
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     batch_size = 2  # mini-batch size
     embedding_size = 2  # embedding size
     sentences = ["apple banana fruit", "banana orange fruit", "orange banana fruit",
                  "dog cat animal", "cat monkey animal", "monkey dog animal"]
+
     "word2id,id2word"
     word_sequence = " ".join(sentences).split()
     word_list = " ".join(sentences).split()
@@ -25,8 +29,7 @@ if __name__ =="__main__":
     word_dict = {w: i for i, w in enumerate(word_list)}
     voc_size = len(word_list)
 
-    # Make skip gram of one size window
-
+    # 当context window size 为1时
     skip_grams = []
     for i in range(1, len(word_sequence) - 1):
         target = word_dict[word_sequence[i]]
@@ -35,12 +38,8 @@ if __name__ =="__main__":
             skip_grams.append([target, w])
     print(word_dict)
     print(len(skip_grams))
-    model = Word2vec(voc_size,embedding_size)
-
-
-
-    criterion = nn.CrossEntropyLoss()
-
+    model = Word2vec(voc_size,embedding_size).to(device)
+    criterion = nn.CrossEntropyLoss().to(device)
     optimizer = optim.Adam(model.parameters(), lr=0.001)
 
 
@@ -51,9 +50,9 @@ if __name__ =="__main__":
 
         input_batch, target_batch = random_batch(skip_grams)
 
-        input_batch = torch.Tensor(input_batch)
+        input_batch = torch.Tensor(input_batch).to(device)
 
-        target_batch = torch.LongTensor(target_batch)
+        target_batch = torch.LongTensor(target_batch).to(device)
 
 
 
@@ -78,12 +77,16 @@ if __name__ =="__main__":
         optimizer.step()
 
 
+    print(word_list)
+    weights=[]
+    for p in model.parameters():
+        weights.append(p)
 
+    wordvec=weights[0]
     for i, label in enumerate(word_list):
 
-        W, WT = model.parameters()
 
-        x, y = W[0][i].item(), W[1][i].item()
+        x, y = wordvec[0][i].item(), wordvec[1][i].item()
 
         plt.scatter(x, y)
 
